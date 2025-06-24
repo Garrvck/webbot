e75344587760bffacc85ac819855283007690321
Garrvck <97791604+Garrvck@users.noreply.github.com>
2025-06-24 15:29:24 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,8 +1,6 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 from aiogram.dispatcher.filters import Command
-# from data.config import OPENAI_KEY
-from handlers.users.word import generate_resume_doc
 from loader import dp
 
 
@@ -12,9 +10,6 @@ async def bot_start(message: types.Message):
 
 @dp.message_handler(Command("start_resume"))
 async def start_resume_handler(message: types.Message):
-    html_link = "https://resume-bot-cfc4560e271d.herokuapp.com/"
-    await message.answer(
-        f"📝 Rezyume to‘ldirish uchun quyidagi havolaga o'ting:\n\n<a href='{html_link}'>➡️ Formani to‘ldirish</a>",
-        disable_web_page_preview=True
-    )
-
+    tg_id = message.from_user.id
+    url = f"https://resume-bot-cfc4560e271d.herokuapp.com/?id={tg_id}"
+    await message.answer(f"👇 Rezyume to‘ldirish uchun sahifani oching:\n{url}")
