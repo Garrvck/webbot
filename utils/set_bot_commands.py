764fd1f2447c3e3562b80698ab0f8abb3ad20f91
Garrvck <97791604+Garrvck@users.noreply.github.com>
2025-06-24 14:12:04 +0500

    Update set_bot_commands.py

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,9 +5,6 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("help", "Yordam"),
-            types.BotCommand("kitob", "Python kitobini xarid qilish"),
-            types.BotCommand("praktikum", "Data Science praktikum xarid qilish"),
-            types.BotCommand("mahsulotlar", "Barcha mahsulotlarni ko'rish"),
+            types.BotCommand("start_resume", "Ma'lumotnoma yaratish"),
         ]
     )
