5949d4d00b76c117dc014d23d5540636cf1e8ef0
Garrvck <97791604+Garrvck@users.noreply.github.com>
2025-06-29 11:40:39 +0500

    Update set_bot_commands.py

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -1,10 +1,10 @@
-from aiogram import types
+# from aiogram import types
 
 
-async def set_default_commands(dp):
-    await dp.bot.set_my_commands(
-        [
-            types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("start_resume", "Ma'lumotnoma yaratish"),
-        ]
-    )
+# async def set_default_commands(dp):
+#     await dp.bot.set_my_commands(
+#         [
+#             types.BotCommand("start", "Botni ishga tushurish"),
+#             types.BotCommand("start_resume", "Ma'lumotnoma yaratish"),
+#         ]
+#     )
