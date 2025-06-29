9f99e968fc32dec5412980756a8d65a5b586a255
Garrvck <97791604+Garrvck@users.noreply.github.com>
2025-06-29 10:14:50 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -23,26 +23,3 @@ async def start_resume_handler(message: types.Message):
     url = f"https://resume-bot-cfc4560e271d.herokuapp.com/?id={tg_id}"
     await message.answer(f"👇 Rezyume to‘ldirish uchun sahifani oching:\n{url}", reply_markup=ReplyKeyboardRemove())
 
-
-@dp.message_handler(content_types=types.ContentType.PHOTO)
-async def handle_photo(msg: types.Message):
-    user_id = msg.from_user.id
-    if user_id not in user_data:
-        await msg.reply("❗ Avval rezyume ma’lumotlarini yuboring.")
-        return
-
-    photo = msg.photo[-1]  # eng katta o'lchamdagi rasm
-    photo_path = f"{user_id}_photo.jpg"
-    await photo.download(destination_file=photo_path)
-
-    resume_dict = user_data.pop(user_id)  # ma’lumotni olamiz va o‘chirib yuboramiz
-    resume_dict["photo_path"] = photo_path
-
-    # Word fayl yaratamiz
-    filename = generate_resume_doc(resume_dict)
-    await msg.answer_document(open(filename, "rb"))
-
-    # Fayllarni tozalash (ixtiyoriy)
-    import os
-    os.remove(photo_path)
-    os.remove(filename)
